886dd0569c02b34773627acf832bf96efb0a088e
annya fraysheht <pedropabloramirezarenas80@gmail.com>
2025-11-14 15:46:26 -0500

    movement (basic)

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,23 +1,24 @@
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-    }
+    public float playerSpeed = 2f;
+    public float horizontalSpeed = 3f;
+    public float leftLimit = 5.5f;
 
-    // Update is called once per frame
-    void Update()
+
+    private void Update()
     {
-        if (Input.GetButtonDown("A"))
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+        if (Input.GetKey(KeyCode.A))
         {
-            transform.position = new Vector3(-10, 0, 0);
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
         }
-        if (Input.GetButtonDown("D"))
+        if (Input.GetKey(KeyCode.D))
         {
-            transform.position = new Vector3(10, 0, 0);
+            transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
         }
     }
 }
